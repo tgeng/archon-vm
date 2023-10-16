377fb42c55bc4a9c32aa17745411516ab58b4fbf
Tianyu Geng <aipser.3d@gmail.com>
2023-10-16 11:45:00 -0700

    fix issues reported by clippy

--- src/term.rs
+++ src/term.rs
@@ -15,8 +15,8 @@ pub enum CTerm {
     Force { thunk: VTerm },
     Let { t: Box<CTerm>, bound_name: String, body: Box<CTerm> },
     Def { name: String },
-    CaseInt { t: VTerm, branches: Box<HashMap<i32, CTerm>>, default_branch: Option<Box<CTerm>> },
+    CaseInt { t: VTerm, branches: HashMap<i32, CTerm>, default_branch: Option<Box<CTerm>> },
     CaseTuple { t: VTerm, bound_names: Vec<String>, branch: Box<CTerm> },
-    CaseStr { t: VTerm, branches: Box<HashMap<String, CTerm>>, default_branch: Option<Box<CTerm>> },
+    CaseStr { t: VTerm, branches: HashMap<String, CTerm>, default_branch: Option<Box<CTerm>> },
     Primitive { name: String, arity: i32 },
 }
--- src/visitor.rs
+++ src/visitor.rs
@@ -7,9 +7,11 @@ pub trait Visitor {
 
     fn combine(&self, results: Vec<Self::Result>) -> Self::Result;
 
-    fn with_binding(&self, ctx: &Self::Context, name: &str) -> Self::Context;
+    fn with_binding<'a>(&self, ctx: &'a mut Self::Context, _name: &str) -> &'a mut Self::Context {
+        ctx
+    }
 
-    fn visit_v_term(&self, ctx: &Self::Context, v_term: &VTerm) -> Self::Result {
+    fn visit_v_term(&self, ctx: &mut Self::Context, v_term: &VTerm) -> Self::Result {
         match v_term {
             VTerm::Var { name } => self.visit_var(ctx, name),
             VTerm::Thunk { t } => self.visit_thunk(ctx, t),
@@ -19,26 +21,26 @@ pub trait Visitor {
         }
     }
 
-    fn visit_var(&self, _ctx: &Self::Context, _name: &str) -> Self::Result {
+    fn visit_var(&self, _ctx: &mut Self::Context, _name: &str) -> Self::Result {
         self.combine(vec! {})
     }
-    fn visit_thunk(&self, ctx: &Self::Context, t: &CTerm) -> Self::Result {
+    fn visit_thunk(&self, ctx: &mut Self::Context, t: &CTerm) -> Self::Result {
         self.visit_c_term(ctx, t)
     }
 
-    fn visit_int(&self, _ctx: &Self::Context, _value: &i32) -> Self::Result {
+    fn visit_int(&self, _ctx: &mut Self::Context, _value: &i32) -> Self::Result {
         self.combine(vec! {})
     }
 
-    fn visit_str(&self, _ctx: &Self::Context, _value: &String) -> Self::Result {
+    fn visit_str(&self, _ctx: &mut Self::Context, _value: &str) -> Self::Result {
         self.combine(vec! {})
     }
 
-    fn visit_tuple(&self, ctx: &Self::Context, values: &Vec<VTerm>) -> Self::Result {
+    fn visit_tuple(&self, ctx: &mut Self::Context, values: &[VTerm]) -> Self::Result {
         self.combine(values.iter().map(|v| self.visit_v_term(ctx, v)).collect())
     }
 
-    fn visit_c_term(&self, ctx: &Self::Context, c_term: &CTerm) -> Self::Result {
+    fn visit_c_term(&self, ctx: &mut Self::Context, c_term: &CTerm) -> Self::Result {
         match c_term {
             CTerm::Lambda { arg_name, body } => self.visit_lambda(ctx, arg_name, body),
             CTerm::App { function, arg } => self.visit_app(ctx, function, arg),
@@ -53,37 +55,37 @@ pub trait Visitor {
         }
     }
 
-    fn visit_lambda(&self, ctx: &Self::Context, arg_name: &str, body: &CTerm) -> Self::Result {
-        self.visit_c_term(&self.with_binding(ctx, arg_name), body)
+    fn visit_lambda(&self, ctx: &mut Self::Context, arg_name: &str, body: &CTerm) -> Self::Result {
+        self.visit_c_term(self.with_binding(ctx, arg_name), body)
     }
 
-    fn visit_app(&self, ctx: &Self::Context, function: &CTerm, arg: &VTerm) -> Self::Result {
+    fn visit_app(&self, ctx: &mut Self::Context, function: &CTerm, arg: &VTerm) -> Self::Result {
         self.combine(vec![
             self.visit_c_term(ctx, function),
             self.visit_v_term(ctx, arg),
         ])
     }
 
-    fn visit_return(&self, ctx: &Self::Context, value: &VTerm) -> Self::Result {
+    fn visit_return(&self, ctx: &mut Self::Context, value: &VTerm) -> Self::Result {
         self.visit_v_term(ctx, value)
     }
 
-    fn visit_force(&self, ctx: &Self::Context, thunk: &VTerm) -> Self::Result {
+    fn visit_force(&self, ctx: &mut Self::Context, thunk: &VTerm) -> Self::Result {
         self.visit_v_term(ctx, thunk)
     }
-    fn visit_let(&self, ctx: &Self::Context, t: &CTerm, bound_name: &str, body: &CTerm) -> Self::Result {
+    fn visit_let(&self, ctx: &mut Self::Context, t: &CTerm, bound_name: &str, body: &CTerm) -> Self::Result {
         self.combine(vec![
             self.visit_c_term(ctx, t),
-            self.visit_c_term(&self.with_binding(ctx, bound_name), body),
+            self.visit_c_term(self.with_binding(ctx, bound_name), body),
         ])
     }
-    fn visit_def(&self, _ctx: &Self::Context, _name: &str) -> Self::Result {
+    fn visit_def(&self, _ctx: &mut Self::Context, _name: &str) -> Self::Result {
         self.combine(vec! {})
     }
 
-    fn visit_case_int(&self, ctx: &Self::Context, t: &VTerm, branches: &HashMap<i32, CTerm>, default_branch: &Option<Box<CTerm>>) -> Self::Result {
+    fn visit_case_int(&self, ctx: &mut Self::Context, t: &VTerm, branches: &HashMap<i32, CTerm>, default_branch: &Option<Box<CTerm>>) -> Self::Result {
         let mut results = vec![self.visit_v_term(ctx, t)];
-        for (_, branch) in branches {
+        for branch in branches.values() {
             results.push(self.visit_c_term(ctx, branch));
         }
         if let Some(default_branch) = default_branch {
@@ -92,17 +94,17 @@ pub trait Visitor {
         self.combine(results)
     }
 
-    fn visit_case_tuple(&self, ctx: &Self::Context, t: &VTerm, bound_names: &Vec<String>, branch: &CTerm) -> Self::Result {
+    fn visit_case_tuple(&self, ctx: &mut Self::Context, t: &VTerm, bound_names: &Vec<String>, branch: &CTerm) -> Self::Result {
         let mut results = vec![self.visit_v_term(ctx, t)];
         for bound_name in bound_names {
-            results.push(self.visit_c_term(&self.with_binding(ctx, bound_name), branch));
+            results.push(self.visit_c_term(self.with_binding(ctx, bound_name), branch));
         }
         self.combine(results)
     }
 
-    fn visit_case_str(&self, ctx: &Self::Context, t: &VTerm, branches: &HashMap<String, CTerm>, default_branch: &Option<Box<CTerm>>) -> Self::Result {
+    fn visit_case_str(&self, ctx: &mut Self::Context, t: &VTerm, branches: &HashMap<String, CTerm>, default_branch: &Option<Box<CTerm>>) -> Self::Result {
         let mut results = vec![self.visit_v_term(ctx, t)];
-        for (_, branch) in branches {
+        for branch in branches.values() {
             results.push(self.visit_c_term(ctx, branch));
         }
         if let Some(default_branch) = default_branch {
@@ -111,7 +113,7 @@ pub trait Visitor {
         self.combine(results)
     }
 
-    fn visit_primitive(&self, _ctx: &Self::Context, _name: &str, _arity: &i32) -> Self::Result {
+    fn visit_primitive(&self, _ctx: &mut Self::Context, _name: &str, _arity: &i32) -> Self::Result {
         self.combine(vec! {})
     }
 }
