eebe7d2ed395fcf73c3601ab1c55237257e7361d
Tianyu Geng <aipser.3d@gmail.com>
2023-10-25 14:23:22 -0700

    partially implement parsers

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,12 @@
 use nom::branch::alt;
 use nom::IResult;
-use nom::character::complete::{line_ending, space0, space1, char, satisfy, alphanumeric1, one_of};
+use nom::character::complete::{line_ending, space0, space1, char, satisfy, alphanumeric1, one_of, newline};
 use nom::combinator::{cut, map, verify};
 use nom::bytes::complete::{take_while1, escaped, tag};
 use nom::error::{context};
 use nom::Parser;
-use nom::multi::{many1, many_m_n, separated_list0};
-use nom::sequence::{delimited, pair, preceded, terminated};
+use nom::multi::{many0, many1, many_m_n, separated_list0};
+use nom::sequence::{delimited, pair, preceded, terminated, tuple};
 use crate::u_term::UTerm;
 use nom_locate::{LocatedSpan};
 
@@ -14,7 +14,7 @@ type Span<'a> = LocatedSpan<&'a str>;
 
 // keywords
 static KEYWORDS: &[&str] = &[
-    "let", "def", "case_int", "case_str", "case_tuple", "force", "thunk",
+    "let", "def", "case_int", "case_str", "case_tuple", "force", "thunk", "="
 ];
 
 fn identifier(input: Span) -> IResult<Span, UTerm> {
@@ -26,7 +26,7 @@ fn identifier(input: Span) -> IResult<Span, UTerm> {
                         .and(take_while1(|c: char| c.is_alphanumeric() || c == '_'))
                         .map(|(head, tail)| format!("{}{}", head, tail)),
                     // punctuation identifier
-                    take_while1(|c: char| c.is_ascii_punctuation() && c != '`' && c != '(' && c != ')' && c != ',').map(|s: Span| s.to_string()),
+                    take_while1(|c: char| c.is_ascii_punctuation() && c != '`' && c != '(' && c != ')' && c != ',' && c != '\\').map(|s: Span| s.to_string()),
                     // backtick-quoted identifier
                     delimited(
                         char('`'),
@@ -68,65 +68,93 @@ fn empty_tuple(input: Span) -> IResult<Span, UTerm> {
     map(tag("(("), |_| UTerm::Tuple { values: vec![] })(input)
 }
 
-struct ParsingContext {
-    indent: usize,
-}
+#[derive(Debug, Clone)]
+struct ParsingContext {}
 
 impl ParsingContext {
-    fn indented<'a, F, R>(&'a self, f: F) -> impl FnOnce(Span<'a>) -> R where F: FnOnce(ParsingContext) -> R {
+    fn indented<'a, F, R>(&'a self, mut f: F) -> impl FnMut(Span<'a>) -> R where F: FnMut(usize, Span<'a>) -> R {
         move |span: Span<'a>| {
-            f(ParsingContext { indent: span.naive_get_utf8_column() })
+            f(span.naive_get_utf8_column(), span)
         }
     }
 
-    fn sp<'a>(&self) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, ()> {
-        alt((
+    fn newline<'a>(&self, indent: usize) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, ()> {
+        map(
             many1(space0.and(line_ending))
-                .and(many_m_n(self.indent, self.indent, char(' ')))
-                .and(many1(char(' '))) // consume additional spaces
-                .map(|_| ()),
-            space1.map(|_| ()),
-        ))
+                .and(many_m_n(indent, indent, char(' ')))
+                .and(many1(char(' '))), // consume additional spaces
+            |_| (),
+        )
+    }
+
+    fn sp<'a>(&self, indent: usize) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, ()> {
+        alt((self.newline(indent), space1.map(|_| ())))
+    }
+
+    fn u_term<'a>(&'a self, indent: usize, inside_parentheses: bool) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
+        move |s: Span<'a>| self.u_term_(s, indent, inside_parentheses)
     }
 
     // Instead of returning an opaque type, we just declare a function to workaround rust's issue
     // with instantiating infinite opaque types due to recursive calls.
-    fn tuple_or_term_<'a>(&'a self, s: Span<'a>, inside_parentheses: bool) -> IResult<Span<'a>, UTerm> {
+    fn u_term_<'a>(&'a self, s: Span<'a>, indent: usize, inside_parentheses: bool) -> IResult<Span<'a>, UTerm> {
+        alt((
+            self.lambda(),
+            self.tuple_or_term(indent, inside_parentheses),
+        ))(s)
+    }
+
+    fn lambda<'a>(&'a self) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
+        self.atom(0)
+    }
+
+    fn tuple_or_term<'a>(&'a self, indent: usize, inside_parentheses: bool) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
         map(
-            separated_list0(delimited(self.sp(), tag(","), self.sp()), self.expr()),
-            |values| {
+            separated_list0(delimited(self.sp(indent), tag(","), self.sp(indent)), self.expr(indent)),
+            move |values: Vec<UTerm>| {
                 if values.len() == 1 && !inside_parentheses {
                     values.into_iter().next().unwrap()
                 } else {
                     UTerm::Tuple { values }
                 }
             },
-        )(s)
-    }
-    fn tuple_or_term<'a>(&'a self, inside_parentheses: bool) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
-        move |s: Span<'a>| self.tuple_or_term_(s, inside_parentheses)
+        )
     }
 
-    fn expr<'a>(&'a self) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
-        self.atom()
+    fn expr<'a>(&'a self, indent: usize) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
+        self.indented_app()
     }
 
-    fn atom<'a>(&'a self) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
+    fn indented_app<'a>(&'a self) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
+        self.indented(|indent, span| {
+            map(
+                tuple((
+                    self.atom(indent),
+                    many0(preceded(self.newline(indent), self.u_term(indent, false))))),
+                |(f, args): (UTerm, Vec<UTerm>)| {
+                    UTerm::App { function: Box::new(f), args }
+                })(span)
+        })
+    }
+
+    fn atom<'a>(&'a self, indent: usize) -> impl FnMut(Span<'a>) -> IResult<Span<'a>, UTerm> {
         alt((
             identifier,
             int,
             str,
             delimited(
-                pair(char('('), self.sp()),
-                self.tuple_or_term(true),
-                pair(self.sp(), char(')')))))
+                pair(char('('), self.sp(indent)),
+                self.u_term(indent, true),
+                pair(self.sp(indent), char(')')))))
     }
 }
 
 
 pub fn parse_u_term(input: &str) -> Result<UTerm, String> {
-    let ctx = ParsingContext { indent: 0 };
-    let r = ctx.tuple_or_term_(Span::new(input), false);
+    let ctx = ParsingContext {};
+    let r = ctx.indented(|indent, span| {
+        ctx.u_term(indent, false)(span)
+    })(Span::new(input));
     match r {
         Ok((_, result)) => Ok(result),
         err => Err(format!("Parse error: {:?}", err)),
