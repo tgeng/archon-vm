7eee385fdb25d10051d068dd92a1e4d8301b6775
Tianyu Geng <aipser.3d@gmail.com>
2023-11-17 17:58:39 -0800

    rename flag

--- runtime/src/runtime.rs
+++ runtime/src/runtime.rs
@@ -1,23 +1,32 @@
+/// Data of unknown representation. This could be a primitive or pointer.
+type Generic = usize;
+/// Data in uniform representation. This is a tagged primitive or pointer.
 type Uniform = usize;
-type FuncPtr = usize;
+/// A function pointer that is not tagged.
+type FuncPtr = *const usize;
+/// A pointer to a continuation implementation, which takes a corrresponding continuation and a
+/// result value and calls the next continuation in the end.
+type ContImplPtr = *const usize;
+/// A pointer to a struct consisting of the effect name and all the arguments to the effect.
+type Eff = *const usize;
 
 
 /// A state object of a function. This also points to the caller state so it effectively represents
 /// a continuation. Hence the name.
 struct Continuation {
     next: *mut Continuation,
-    func: *const FuncPtr,
-    progress: usize,
-    // local variables follows here right after this struct
+    func: ContImplPtr,
+    state: usize,
+    local_vars: *mut [Generic],
 }
 
 struct Handler {
     parent: *mut Handler,
     transform_continuation: *mut Continuation,
     parameter: *mut Uniform,
-    parameter_disposer: *const FuncPtr,
-    parameter_replicator: *const FuncPtr,
-    transform: *const FuncPtr,
-    simple_handler: Vec<(Uniform, *const FuncPtr)>,
-    complex_handler: Vec<(Uniform, *const FuncPtr)>,
+    parameter_disposer: FuncPtr,
+    parameter_replicator: FuncPtr,
+    transform: FuncPtr,
+    simple_handler: Vec<(Eff, FuncPtr)>,
+    complex_handler: Vec<(Eff, FuncPtr)>,
 }
